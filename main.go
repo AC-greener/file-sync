9dad928f957ae91503d7801da78edbdf32e0a816
zhutongtong <zhutongtong@360.cn>
2023-07-22 10:41:21 +0800

    重构：把gin服务移动到/server

--- main.go
+++ main.go
@@ -1,185 +1,44 @@
 package main
 
 import (
-	"embed"
 	"fmt"
-	"io/fs"
-	"io/ioutil"
 	"log"
-	"net"
-	"net/http"
 	"os"
 	"os/signal"
-	"path/filepath"
-	"strings"
 	"syscall"
+	"test/server"
 
-	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"github.com/skip2/go-qrcode"
 	"github.com/zserge/lorca"
 )
 
-//go:embed frontend/dist/*
-var FS embed.FS
-
 func main() {
 	fmt.Println("Hello, World!")
-	go func() {
-		gin.SetMode(gin.DebugMode)
-		router := gin.Default()
-		staticFiles, _ := fs.Sub(FS, "frontend/dist")
-		router.StaticFS("/static", http.FS(staticFiles))
-		router.POST("/api/v1/texts", TextsController)
-		router.GET("/api/v1/addresses", AddressController)
-		router.GET("/uploads/:path", UploadController)
-		router.GET("/api/v1/qrcodes", QrcodesController)
-		router.POST("/api/v1/files", FilesController)
-		router.NoRoute(func(c *gin.Context) {
-			path := c.Request.URL.Path
-			if strings.HasPrefix(path, "/static/") {
-				reader, err := staticFiles.Open("index.html")
-				if err != nil {
-					log.Fatal(err)
-				}
-				defer reader.Close()
-				stat, err := reader.Stat()
-				if err != nil {
-					log.Fatal(err)
-				}
-				c.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil)
-			} else {
-				c.Status(http.StatusNotFound)
-			}
-		})
-		router.Run(":27149")
+	go server.Run()
 
-	}()
-
-	// Create UI with basic HTML passed via data URI
-	ui, err := lorca.New("http://127.0.0.1:27149/static/index.html", "", 480, 320, "--remote-allow-origins=*")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ui.Close()
-
+	ui := startChrome()
 	// chromePath := "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 	// cmd := exec.Command(chromePath, "--app=http://127.0.0.1"27149")
 	// cmd.Start()
-	//处理中断和终止信号
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	c := listenToInterupt()
+	defer ui.Close()
 
 	//等第一个可以读或可以写的channel进行操作
 	select {
 	case <-c:
 	case <-ui.Done():
 	}
-	// Wait until UI window is closed
 }
-
-func TextsController(c *gin.Context) {
-	var json struct {
-		Raw string `json:"raw" binding:"required"`
-	}
-	if err := c.ShouldBindJSON(&json); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	} else {
-		exe, err := os.Executable() //获取当执行路径
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		dir := filepath.Dir(exe) // 获取当前执行文件的目录
-		filename := uuid.New().String()
-		uploads := filepath.Join(dir, "uploads") // 拼接 uploads 的绝对路径
-		err = os.MkdirAll(uploads, os.ModePerm)  // 创建 uploads 目录
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		fullpath := filepath.Join("uploads", filename+".txt")                        // 拼接文件的绝对路径（不含 exe 所在目录）
-		err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644) // 将 json.Raw 写入文件
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
-	}
-}
-func FilesController(c *gin.Context) {
-	file, err := c.FormFile("raw")
+func startChrome() lorca.UI {
+	ui, err := lorca.New("http://127.0.0.1:27149/static/index.html", "", 480, 320, "--remote-allow-origins=*")
 	if err != nil {
 		log.Fatal(err)
 	}
-	exe, err := os.Executable() //获取当执行路径
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	dir := filepath.Dir(exe) // 获取当前执行文件的目录
-	filename := uuid.New().String()
-	uploads := filepath.Join(dir, "uploads") // 拼接 uploads 的绝对路径
-	err = os.MkdirAll(uploads, os.ModePerm)  // 创建 uploads 目录
-	if err != nil {
-		log.Fatal(err)
-	}
-	fullpath := filepath.Join("uploads", filename+filepath.Ext(file.Filename)) // 拼接文件的绝对路径（不含 exe 所在目录）
-	fileErr := c.SaveUploadedFile(file, filepath.Join(dir, fullpath))
-	if fileErr != nil {
-		log.Fatal(fileErr)
-	}
-	c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
-
+	return ui
 }
 
-func AddressController(c *gin.Context) {
-	addrs, _ := net.InterfaceAddrs()
-	var result []string
-	for _, addr := range addrs {
-		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				result = append(result, ipnet.IP.String())
-			}
-		}
-	}
-	c.JSON(http.StatusOK, gin.H{"addresses": result})
-}
-
-func GetUploadsDir() (uploads string) {
-	exe, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
-	}
-	dir := filepath.Dir(exe)
-	uploads = filepath.Join(dir, "uploads")
-	return
-}
-
-// 从前端接口的参数里面获取要下载的文件名，然后返给前端
-func UploadController(c *gin.Context) {
-	if path := c.Params.ByName("path"); path != "" {
-		target := filepath.Join(GetUploadsDir(), path)
-		c.Header("Content-Description", "File Transfer")
-		c.Header("Content-Transfer-Encoding", "binary")
-		c.Header("Content-Disposition", "attachment; filename="+path)
-		c.Header("Content-Type", "application/octet-stream")
-		c.File(target)
-	} else {
-		c.Status(http.StatusNotFound)
-	}
-}
-
-// 生成二维码
-func QrcodesController(c *gin.Context) {
-	if content := c.Query("content"); content != "" {
-		png, err := qrcode.Encode(content, qrcode.Medium, 256)
-		if err != nil {
-			log.Fatal(err)
-		}
-		c.Data(http.StatusOK, "image/png", png)
-	} else {
-		c.Status(http.StatusNotFound)
-	}
+func listenToInterupt() chan os.Signal {
+	//处理中断和终止信号
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	return c
 }
